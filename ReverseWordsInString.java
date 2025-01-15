5063b433be5a7e225d4c92b8d9cec4c58c462a12
edwinjoel1202 <edwinjoel1204@gmail.com>
2025-01-15 22:41:32 +0530

    151. Reverse Words in a String

---
+++ ReverseWordsInString.java
@@ -0,0 +1,17 @@
+import java.lang.StringBuilder;
+
+class ReverseWordsInString {
+    public String reverseWords(String s) {
+        String[] arr = s.trim().split("\\s+");
+        StringBuilder str = new StringBuilder();
+
+        for(int i = arr.length - 1; i >= 0; i--){
+            str.append(arr[i]);
+            if(i != 0){
+                str.append(" ");
+            }
+        }
+
+        return str.toString();
+    }
+}
