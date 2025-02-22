0dd9f5507e9bffe3137894c308269dfbc08f8cdf
edwinjoel1202 <edwinjoel1204@gmail.com>
2025-02-22 20:29:14 +0530

    1539. Kth Missing Postive Number

---
+++ KthMissingPositiveNumber.java
@@ -0,0 +1,13 @@
+class KthMissingPositiveNumber {
+    public int findKthPositive(int[] arr, int k) {
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] <= k){
+                k++;
+            }
+            else{
+                break;
+            }
+        }
+        return k;
+    }
+}
