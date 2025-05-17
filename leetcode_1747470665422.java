84b073b32b858ed6bb809bf508c92cf411a926dd
Edwin Joel J <142042568+edwinjoel1202@users.noreply.github.com>
2025-05-17 14:01:22 +0530

    2985. Find Missing and Repeated Values

---
+++ leetcode_1747470665422.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] findMissingAndRepeatedValues(int[][] grid) {
+        int n = grid.length;
+        int nSquared = n * n;
+        int[] count = new int[nSquared + 1];
+
+        for (int[] row : grid) {
+            for (int num : row) {
+                count[num]++;
+            }
+        }
+
+        int repeated = -1;
+        int missing = -1;
+        
+        for (int i = 1; i <= nSquared; i++) {
+            if (count[i] == 2) {
+                repeated = i;
+            } else if (count[i] == 0) {
+                missing = i;
+            }
+        }
+
+        return new int[]{repeated, missing};
+    }
+}
